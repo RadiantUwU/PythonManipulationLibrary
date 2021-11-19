5cb3c104a2d162ec7f380dd6192e4c79a3c58ed5
RadiantUwU <69520693+RadiantUwU@users.noreply.github.com>
2021-11-19 20:08:55 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,1 +1,24 @@
-__version__ = '0.0.0.2'
+# noinspection PyUnresolvedReferences
+import coloring
+# noinspection PyUnresolvedReferences
+import getch
+# noinspection PyUnresolvedReferences
+import logger
+# noinspection PyUnresolvedReferences
+import mathMisc
+# noinspection PyUnresolvedReferences
+import modifiableSystems
+# noinspection PyUnresolvedReferences
+import moduleReloader
+# noinspection PyUnresolvedReferences
+import nullObj
+# noinspection PyUnresolvedReferences
+import pyCObj
+# noinspection PyUnresolvedReferences
+import sockets
+# noinspection PyUnresolvedReferences
+import timestuff
+
+# imports everything
+
+__version__ = '0.0.0.3'
